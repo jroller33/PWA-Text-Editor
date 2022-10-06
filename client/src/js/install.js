2b5b0d86f560803ca823d9f18af471a45f8e0183
John Roller <jtroller33@gmail.com>
2022-10-06 00:19:49 -0500

    everything in src folder done

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -16,5 +16,6 @@ butInstall.addEventListener('click', async () => {
     btnInstallEl.classList.toggle('hidden', true);
 });
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => { });
+window.addEventListener('appinstalled', (event) => {
+    window.deferredPrompt = null;
+});
